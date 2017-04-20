fc30773342389feadb50a39a018f70ef258d97fa
Andre L. Martos <andremartos@movida.com.br>
2017-04-20 16:48:54 -0300

    #

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ function dias_feriados($ano = null)
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Idleness Systems</title>
+  <title>Idleness Center</title>
   <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css"> -->
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
