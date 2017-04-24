916decf847adafa83957c25659294be03720e690
Andre L. Martos <andremartos@movida.com.br>
2017-04-24 14:19:17 -0300

    #JQUERY

--- index.php
+++ index.php
@@ -86,8 +86,8 @@ function dias_feriados($ano = null)
   <title>Idleness Center</title>
   <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css"> -->
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="jquery_1.12.4/jquery-1.12.4.js"></script>
+  <script src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>  
   <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
   <link href="bootstrap/css/jquery.dynameter.css" rel="stylesheet" type="text/css" />
   <script src="bootstrap/js/bootstrap.js"></script>
@@ -694,7 +694,7 @@ function dias_feriados($ano = null)
               </div>              
             </div>
           </div>
-        </div-->
+        </div>
         
         <!--div class="modal fade" tabindex="-1" role="dialog" id='racer'>
           <div class="modal-dialog modal-lg" role="document">
@@ -747,7 +747,9 @@ function dias_feriados($ano = null)
               </div>
             </div><!-- /.modal-content -->
           </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->               
+        </div><!-- /.modal --> 
+        
+                      
     </body>
 
 </html>
