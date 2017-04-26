359f12fae9820c9ba422e5618f062286a49ee982
Andre L. Martos <andremartos@movida.com.br>
2017-04-26 09:26:38 -0300

    cursor pointer fechar do chat

--- chat/chat.js
+++ chat/chat.js
@@ -2,7 +2,7 @@ var showchat = true;
 
 function callchat() {
 	$('#chat').remove();
-	$('body').append( "<div id='chat'><div class='header'><div style='padding-right:5px'>Fechar</div></div><iframe width=100% height=100% frameborder=0 src='chat/' framescroll='no' ></iframe></div>" )
+	$('body').append( "<div id='chat'><div class='header'><div style='padding-right:5px; cursor:pointer'>Fechar</div></div><iframe width=100% height=100% frameborder=0 src='chat/' framescroll='no' ></iframe></div>" )
 	var css = {	position: 'absolute',
 				bottom: '0px',
 				width: '250px',
